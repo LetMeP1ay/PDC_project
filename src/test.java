da4aa44000609a6729341086acf11b7e74bc892a
LetMeP1ay <vdaberezin@gmail.com>
2024-06-11 15:08:29 +1200

    Edited the databaseURL, ensured that the connection works

--- src/test.java
+++ src/test.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
  */
 public class test {
     public static void main(String[] args) {
-        String databaseURL = "jbdc:derby:OSS_DB";
+        String databaseURL = "jdbc:derby:/Users/letmeplay/NetBeansProjects/PDC_project/OSS_DB";
         try {
             Connection connection = DriverManager.getConnection(databaseURL);
             
